af4c98b957ea001601ff793d5928985b1d1865f1
Subhamstar <mr.subhamdas999@gmail.com>
2025-10-21 12:34:41 +0530

    some testing

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,13 @@
 const express=require('express');
 const app=express();
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+app.use("/user",(req,res,next)=>{
+    console.log("!st response");
+    // res.send(" Hello World from 1st !! ");
+    next();
+},(req,res)=>{
+    console.log("2sd response");
+    res.send("hello world fron 2nd !! ");
+})
+app.listen(7777,()=>{
+    console.log("Server is running on port 7777");
 })
