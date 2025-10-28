4093102c20adf9770810aeaa84686aa1032e17a3
Subhamstar <mr.subhamdas999@gmail.com>
2025-10-28 13:26:13 +0530

    feed

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,39 @@ const express=require('express');
 const mongoose=require("mongoose")
 const conncetDB=require("./src/config/db");
 const app=express();
+const User=require("./src/models/user")
+
+app.use(express.json());
+
+app.post("/signUp",async (req,res)=>{
+    // console.log(req.body);
+    try{
+        const {firstName,lastName,email,password}=req.body;
+        const user=new User({
+            firstName,
+            lastName,
+            email,
+            password,
+        })
+        await user.save();
+        res.send("User Added SuccessFully !! ");    
+    }
+    catch(err){
+        res.status(400).send("Error while SignUp data : "+err.message);
+    }
+})
+
+app.get("/feed",async (req,res)=>{
+    try{
+        const user=await User.find({});
+        res.send(user);
+    }
+    catch(err){
+        res.status(400)
+    }
+})
+
+
 conncetDB()
     .then((conn)=>{
         console.log(`Mongo db connect at : ${conn.connection.host} 🚀`);
--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,27 +1,42 @@
 const mongoose=require("mongoose");
+const validator=require("validator");
 const userSchema=new mongoose.Schema({
     firstName:{
         type:String,
-        require:true,
+        required:true,
+        trim:true,
     },
     lastName:{
         type:String,
+        trim:true,
     },
     email:{
         type:String,
-        require:true,
+        required:true,
+        unique:true,
+        validate:{
+            validator:validator.isEmail,
+            message:"Please enter a valid email id ",
+        }
     },
     password:{
         type:String,
-        require:true,
+        required:true,
+        trim:true,
+        validate:{
+            validator:validator.isStrongPassword,
+            message:"Please provide a strong password",
+        }
     },
     age:{
         type:Number,
     },
     gender:{
         type:String,
+    },
+    skills:{
+        type:[String],
     }
-
-})
+},{timestamps:true})
 const userModel=mongoose.model("User",userSchema);
 module.exports=userModel;
